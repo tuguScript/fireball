4c8d7113288d0e45ecab96a24434a95cd62ca2d9
Tuguldur Bayarnyam <tuguldur@Tuguldurs-MacBook-Pro.local>
2018-03-26 17:16:11 -0700

    add Signout, Registration function

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Button from '../components/Button';
-import firebase from '../lib/firebase';
 
 export default class Feed extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
-        {/* <Button title="Go to Details" onPress={() => this.props.navigation.navigate('Details')} /> */}
-        <Button text="Load user" onPress={() => this.loadUser()} />
         <Button text="Go to Details" onPress={() => this.props.navigation.navigate('Details')} />
-        <Button text="Go to Register" onPress={() => this.props.navigation.navigate('Register')} />
-        <Button text="Go to Login" onPress={() => this.props.navigation.navigate('Login')} />
-        <Button
-          text="Sign out"
-          onPress={() =>
-            firebase
-              .auth()
-              .signOut()
-              .then(
-                () => console.log('Signed Out'),
-                (error) => {
-                  console.error('Sign Out Error', error);
-                },
-              )
-          }
-        />
       </View>
     );
   }
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Button from '../components/Button';
+import firebase from '../lib/firebase';
 
 export default class Profile extends React.Component {
-  state = {};
+  constructor() {
+    super();
+    this._bootstrapAsync();
+    this.state = {
+      currentUser: {
+        email: 'tug',
+      },
+    };
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    firebase.auth().onAuthStateChanged(user => {
+      let currentUser = firebase.auth().currentUser;
+      let name, email, photoUrl, uid, emailVerified;
+
+      if (currentUser != null) {
+        console.log(currentUser);
+        this.setState({ currentUser });
+        email = currentUser.email;
+        photoUrl = currentUser.photoURL;
+        emailVerified = currentUser.emailVerified;
+        uid = currentUser.uid; // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+      }
+    });
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile</Text>
+        <Text>{this.state.currentUser.email}</Text>
+        <Button
+          text="Sign out"
+          onPress={() =>
+            firebase
+              .auth()
+              .signOut()
+              .then(
+                () => console.log('Signed Out'),
+                error => {
+                  console.error('Sign Out Error', error);
+                },
+              )
+          }
+        />
       </View>
     );
   }
