f26f674a223074cf394c192d4d3b36f9013eb018
Tuguldur Bayarnyam <tuguldur@Tuguldurs-MacBook-Pro.local>
2018-03-29 17:23:14 -0700

    Implement profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import Button from '../components/Button';
 import firebase from '../lib/firebase';
+import { RkStyleSheet, RkText } from 'react-native-ui-kitten';
 
 export default class Profile extends React.Component {
   constructor() {
@@ -35,23 +36,100 @@ export default class Profile extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{this.state.currentUser.email}</Text>
-        <Button
-          text="Sign out"
-          onPress={() =>
-            firebase
-              .auth()
-              .signOut()
-              .then(
-                () => console.log('Signed Out'),
-                error => {
-                  console.error('Sign Out Error', error);
-                },
-              )
-          }
-        />
-      </View>
+      <ScrollView style={styles.root}>
+        <View style={[styles.header, styles.bordered]}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: `${this.state.currentUser.photoURL}`,
+            }}
+          />
+          <RkText rkType="header2">{this.state.currentUser.displayName}</RkText>
+          <RkText rkType="header2">{this.state.currentUser.email}</RkText>
+          <Button
+            text="Sign out"
+            onPress={() =>
+              firebase
+                .auth()
+                .signOut()
+                .then(
+                  () => console.log('Signed Out'),
+                  error => {
+                    console.error('Sign Out Error', error);
+                  },
+                )
+            }
+          />
+        </View>
+
+        <View style={styles.userInfo}>
+          <View style={styles.section}>
+            <RkText rkType="header3" style={styles.space}>
+              {`1`}
+            </RkText>
+            <RkText rkType="secondary1 hintColor">Posts</RkText>
+          </View>
+          <View style={styles.section}>
+            <RkText rkType="header3" style={styles.space}>
+              {`3`}
+            </RkText>
+            <RkText rkType="secondary1 hintColor">Followers</RkText>
+          </View>
+          <View style={styles.section}>
+            <RkText rkType="header3" style={styles.space}>
+              {`4`}
+            </RkText>
+            <RkText rkType="secondary1 hintColor">Following</RkText>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+let styles = RkStyleSheet.create(theme => ({
+  root: {
+    backgroundColor: theme.colors.screen.base,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  header: {
+    alignItems: 'center',
+    paddingTop: 25,
+    paddingBottom: 17,
+  },
+  userInfo: {
+    flexDirection: 'row',
+    paddingVertical: 18,
+  },
+  bordered: {
+    borderBottomWidth: 1,
+    borderColor: theme.colors.border.base,
+  },
+  section: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  space: {
+    marginBottom: 3,
+  },
+  separator: {
+    backgroundColor: theme.colors.border.base,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    flex: 0,
+    width: 1,
+    height: 42,
+  },
+  buttons: {
+    flexDirection: 'row',
+    paddingVertical: 8,
+  },
+  button: {
+    marginTop: 18,
+    alignSelf: 'center',
+    width: 140,
+  },
+}));
